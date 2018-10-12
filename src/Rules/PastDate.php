0e546bec1edde6bb20717c0aec3772d7733a796d
Brent Roose <brent@spatie.be>
2018-10-12 11:07:01 +0200

    Add PastDate

---
+++ src/Rules/PastDate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Spatie\ValidationRules\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Carbon;
+
+class PastDate implements Rule
+{
+    public function passes($attribute, $value)
+    {
+        $date = Carbon::make($value);
+
+        return $date->isPast();
+    }
+
+    public function message()
+    {
+        return __('validationRules.past_date');
+    }
+}
