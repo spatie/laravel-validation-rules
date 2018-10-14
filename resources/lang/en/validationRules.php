ca3fca4663243f715ae2160b5e476b42e5b5776a
Freek Van der Herten <freek@spatie.be>
2018-10-14 22:59:39 +0200

    Update validationRules.php

--- resources/lang/en/validationRules.php
+++ resources/lang/en/validationRules.php
@@ -3,5 +3,5 @@
 return [
     'authorized' => 'You are not authorized to use this value.',
     'enum' => 'This is not a valid value.',
-    'model_ids' => 'Some of the given ids do not exist',
+    'model_ids' => 'Some of the given ids do not exist.',
 ];
