1d6ee8a0d0f61400dc830ffdcc7c490c38377d2e
Freek Van der Herten <freek@spatie.be>
2019-02-19 21:41:44 +0000

    Apply fixes from StyleCI

--- resources/lang/en/validationRules.php
+++ resources/lang/en/validationRules.php
@@ -4,5 +4,5 @@ return [
     'authorized' => 'You are not authorized to use this value.',
     'enum' => 'This is not a valid value.',
     'model_ids' => 'Some of the given ids do not exist.',
-    'country_code' => 'Invalid country code.'
+    'country_code' => 'Invalid country code.',
 ];
